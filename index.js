d73958ad1fb0218c6ea46d4c7e255d14d7f7c74b
Spencer Hahmo <schahmo@gmail.com>
2023-02-18 14:55:42 -0700

    Cleaned up the code

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const printPassTimes = (passTimes) => {
     const dateTime = new Date(0);
     dateTime.setUTCSeconds(passes.risetime);
     const duration = passes.duration;
-    console.log(`Next pass at ${dateTime} for ${duration} seconds!`)
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
   }
 };
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) return console.log("There was an error!", error);
   printPassTimes(passTimes);
-})
+});
