a73b9405544f35ff1a3c515fd6f61993539e3671
Spencer Hahmo <schahmo@gmail.com>
2023-02-17 20:10:59 -0700

    Gets the IP of the user and outputs it/ an error message if an errror occurs

---
+++ iss.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      return callback(error, null);
+    }
+    
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    
+    const myIPAddress = JSON.parse(body).ip;
+    callback(null, myIPAddress);
+  });
+};
+
+module.exports = { fetchMyIP };
